024c12b46595f782c80fdaaef2f4929aa21c38d7
Rachelll <142870008+rayrayrachel@users.noreply.github.com>
2024-10-30 17:01:04 +0000

    [CODE] Initialise xml,klt files

--- app/src/main/java/com/example/examsos/MainActivity.kt
+++ app/src/main/java/com/example/examsos/MainActivity.kt
@@ -1,22 +1,62 @@
 package com.example.examsos
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+    private val myTag = "RachelsTag"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonClick = findViewById<Button>(R.id.button3)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
         }
+
+        Log.i(myTag, "*** Ray: In onCreate")
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(myTag, "*** Ray: In onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(myTag, "*** Ray: In onStop")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(myTag, "*** Ray: In onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(myTag, "*** Ray: In onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(myTag, "*** Ray: In onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(myTag, "*** Ray: In onDestroy")
+    }
+
 }
 
 //Test Commit
---
+++ app/src/main/java/com/example/examsos/QuizActivity.kt
@@ -0,0 +1,20 @@
+package com.example.examsos
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class QuizActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+        
+    }
+
+}
+
+
+
+
+
